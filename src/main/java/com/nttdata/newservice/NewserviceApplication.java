c734be5d62ffdab6ada3d12a281a7e84bbf19660
gowsmohiddin <52192379+gowsmohiddin@users.noreply.github.com>
2019-07-09 15:52:52 +0530

    first commit

---
+++ src/main/java/com/nttdata/newservice/NewserviceApplication.java
@@ -0,0 +1,14 @@
+package com.nttdata.newservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewserviceApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/main/java/com/nttdata/newservice/ServiceController.java
@@ -0,0 +1,10 @@
+package com.nttdata.newservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceController {
+	
+ 
+
+}
